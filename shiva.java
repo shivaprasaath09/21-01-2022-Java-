75777d8770eae169f1790366dd40bd61542421c6
shivaprasaath09 <75976441+shivaprasaath09@users.noreply.github.com>
2022-01-21 12:46:37 +0530

    Add files via upload

---
+++ shiva.java
@@ -0,0 +1,106 @@
+
+package OnlineClasses;
+
+import java.util.Scanner;
+
+public class shiva 
+{
+	//1st qn
+	
+	/* public static void main(String[] args) {
+        int a[];
+        int n;
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter size: ");
+        n=s.nextInt();
+        a=new int[n];
+        System.out.println("Enter array: ");
+        for(int i=0;i<n;i++)
+            a[i]=s.nextInt();
+        System.out.println("Printing array: ");
+        for(int i=0;i<n;i++)
+            System.out.print(a[i]+" ");
+    }   */
+	
+	//2n qn    
+	/*public static void main(String[] args)
+	    {
+	        int a[];
+	        int n;
+	        Scanner s=new Scanner(System.in);
+	        System.out.print("Enter size: ");
+	        n=s.nextInt();
+	        a=new int[n];
+	        System.out.println("Enter array: ");
+	        for(int i=0;i<n;i++)
+	            a[i]=s.nextInt();
+	        System.out.println("Printing array in reverse order: ");
+	        for(int i=n-1;i>=0;i--)
+	            System.out.print(a[i]+" ");
+	    }  */
+	
+	//3rd Qn
+	
+	/*public static void main(String[] args) {
+        int a[];
+        int n;
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter size: ");
+        n=s.nextInt();
+        a=new int[n];
+        System.out.println("Enter array: ");
+        for(int i=0;i<n;i++)
+            a[i]=s.nextInt();
+            System.out.println("Duplicates: ");
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++)
+                if(a[i]==a[j])
+                    System.out.println(a[j]);
+        }
+    } */
+	
+	//4th Qn
+	
+	 /*public static void main(String[] args) {
+	        int a[];
+	        int n;
+	        Scanner s=new Scanner(System.in);
+	        System.out.print("Enter size: ");
+	        n=s.nextInt();
+	        a=new int[n];
+	        System.out.println("Enter array: ");
+	        for(int i=0;i<n;i++)
+	            a[i]=s.nextInt();
+	        System.out.println("After sorting: ");
+	        for(int i=0;i<n-1;i++)
+	        {
+	            int min = i;
+	            for (int j=i+1;j<n;j++)
+	                if (a[j]<a[min])
+	                    min=j;
+	            int temp=a[min];
+	            a[min]=a[i];
+	            a[i]=temp;
+	        }
+	        for(int i=0;i<n;i++)
+	            System.out.print(a[i]+" ");
+	    } */
+	
+	//5th Qn
+	
+	/* public static void main(String[] args) {
+	        int n;
+	        Scanner s=new Scanner(System.in);
+	        System.out.print("Enter number: ");
+	        n=s.nextInt();
+	        int num=0;
+	        while (n>0) {
+	            num=num*10+n%10;
+	            n=n/10;
+	        }
+	        System.out.println("Reversed number:"+"\n"+num);
+	    } */
+	
+	
+	}
+
